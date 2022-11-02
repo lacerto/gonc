0555e50bcf074ee4d96179d70dc7946cd5e9acdc
glaciurso <glaciurso@sdfeu.org>
2022-11-02 19:25:07 +0100

    Print file modification time

--- gonc.c
+++ gonc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fts.h>
@@ -28,8 +29,6 @@ int isdir(char const*const path) {
 }
 
 int main(int argc, char* argv[argc+1]) {
-    char* source_path;
-    char const* destination_path;
     FTS* file_hierarchy;
     FTSENT* file;
 
@@ -38,17 +37,17 @@ int main(int argc, char* argv[argc+1]) {
         return EXIT_FAILURE;
     }
 
-    source_path = argv[1];
-    // Remove trailing '/' if there is one.
+    char* const source_path = argv[1];
+    char* const destination_path = argv[2];
+
+    // Remove trailing '/' from source path if there is one.
     if (source_path[strlen(source_path) - 1] == '/') 
         source_path[strlen(source_path) - 1] = '\0';
-
-    destination_path = argv[2];
 
     if (isdir(source_path) == -1) return EXIT_FAILURE;
     if (isdir(destination_path) == -1) return EXIT_FAILURE;
 
-    char* const path[2] = { (char* const)source_path, NULL };
+    char* const path[2] = { source_path, NULL };
     file_hierarchy = fts_open(path, FTS_LOGICAL, NULL);
     if (errno) {
         perror(NULL);
@@ -59,6 +58,8 @@ int main(int argc, char* argv[argc+1]) {
         if (file->fts_info == FTS_F) {
             printf("path: %s\nname: %s\n", file->fts_path, file->fts_name);
             printf("pathlen: %i\nnamelen: %i\n", file->fts_pathlen, file->fts_namelen);
+            time_t const* mtime = &file->fts_statp->st_mtime;
+            printf("%s\n", ctime(mtime));
         }
     }
 
